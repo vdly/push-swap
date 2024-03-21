4250d461aae9af887848db96467ab285b51cb5ad
Jodene Hii <johii@u91z05s01.42kl.edu.my>
2024-03-21 19:05:38 +0800

    done i think

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: johii <johii@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/15 19:04:28 by johii             #+#    #+#             */
-/*   Updated: 2024/03/21 18:50:23 by johii            ###   ########.fr       */
+/*   Updated: 2024/03/21 19:05:13 by johii            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,61 +17,6 @@ int	main(int ac, char **av)
 	t_list	*stack_a;
 	t_list	*stack_b;
 	char	**args;
-	int		i;
-	int		j;
-
-	i = 0;
-	stack_a = NULL;
-	stack_b = NULL;
-	if (ac == 1)
-		return (0);
-	if (ac == 2)
-		args = ft_split(av[1], ' ');
-	else
-		args = av + 1;
-	if (!error_check(args))
-		return (1);
-	while (args[i] != NULL)
-		add_last(&stack_a, add_new(ft_atoi(args[i++])));
-	if (ac == 2)
-	{
-		j = 0;
-		while (args[j] != NULL)
-			free(args[j++]);
-		free(args);
-	}
-	indexing(stack_a);
-	simple_sorts(&stack_a, &stack_b, lst_size(stack_a));
-	if (!sorted(stack_a))
-		radix_sort(&stack_a, &stack_b);
-	lst_clear(&stack_a);
-	lst_clear(&stack_b);
-}
-
-void	init_stack(t_list *stack_a, int ac, char **av)
-{
-	int		i;
-	char	**args;
-
-	i = 0;
-	if (ac == 2)
-		args = ft_split(av[i], ' ');
-	else
-	{
-		i = 1;
-		args = av;
-	}
-	while (args[i++])
-		add_last(&stack_a, add_new(ft_atoi(av[i])));
-	if (ac == 2)
-		lst_clear(&stack_a);
-}
-
-int	main(int ac, char **av)
-{
-	t_list	*stack_a;
-	t_list	*stack_b;
-	char	**args;
 
 	stack_a = NULL;
 	stack_b = NULL;
@@ -83,7 +28,7 @@ int	main(int ac, char **av)
 		args = av + 1;
 	if (!error_check(args))
 		return (1);
-	stack_init(ac, args, stack_a);
+	stack_init(ac, args, &stack_a);
 	indexing(stack_a);
 	simple_sorts(&stack_a, &stack_b, lst_size(stack_a));
 	if (!sorted(stack_a))
@@ -92,14 +37,14 @@ int	main(int ac, char **av)
 	lst_clear(&stack_b);
 }
 
-void	stack_init(int ac, char **args, t_list *stack_a)
+void	stack_init(int ac, char **args, t_list **stack_a)
 {
 	int		i;
 	int		j;
 
 	i = 0;
 	while (args[i] != NULL)
-		add_last(&stack_a, add_new(ft_atoi(args[i++])));
+		add_last(stack_a, add_new(ft_atoi(args[i++])));
 	if (ac == 2)
 	{
 		j = 0;
@@ -108,4 +53,3 @@ void	stack_init(int ac, char **args, t_list *stack_a)
 		free(args);
 	}
 }
-

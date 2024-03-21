9a710a73e2fd70131f41e9330a583860b5fa36fd
Jodene Hii <johii@u90z03s03.42kl.edu.my>
2024-03-21 18:51:34 +0800

    uhh

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: johii <johii@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/15 19:04:28 by johii             #+#    #+#             */
-/*   Updated: 2024/03/20 18:29:10 by johii            ###   ########.fr       */
+/*   Updated: 2024/03/21 18:50:23 by johii            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ int	main(int ac, char **av)
 	t_list	*stack_b;
 	char	**args;
 	int		i;
+	int		j;
 
 	i = 0;
 	stack_a = NULL;
@@ -32,6 +33,13 @@ int	main(int ac, char **av)
 		return (1);
 	while (args[i] != NULL)
 		add_last(&stack_a, add_new(ft_atoi(args[i++])));
+	if (ac == 2)
+	{
+		j = 0;
+		while (args[j] != NULL)
+			free(args[j++]);
+		free(args);
+	}
 	indexing(stack_a);
 	simple_sorts(&stack_a, &stack_b, lst_size(stack_a));
 	if (!sorted(stack_a))
@@ -58,3 +66,46 @@ void	init_stack(t_list *stack_a, int ac, char **av)
 	if (ac == 2)
 		lst_clear(&stack_a);
 }
+
+int	main(int ac, char **av)
+{
+	t_list	*stack_a;
+	t_list	*stack_b;
+	char	**args;
+
+	stack_a = NULL;
+	stack_b = NULL;
+	if (ac == 1)
+		return (0);
+	if (ac == 2)
+		args = ft_split(av[1], ' ');
+	else
+		args = av + 1;
+	if (!error_check(args))
+		return (1);
+	stack_init(ac, args, stack_a);
+	indexing(stack_a);
+	simple_sorts(&stack_a, &stack_b, lst_size(stack_a));
+	if (!sorted(stack_a))
+		radix_sort(&stack_a, &stack_b);
+	lst_clear(&stack_a);
+	lst_clear(&stack_b);
+}
+
+void	stack_init(int ac, char **args, t_list *stack_a)
+{
+	int		i;
+	int		j;
+
+	i = 0;
+	while (args[i] != NULL)
+		add_last(&stack_a, add_new(ft_atoi(args[i++])));
+	if (ac == 2)
+	{
+		j = 0;
+		while (args[j] != NULL)
+			free(args[j++]);
+		free(args);
+	}
+}
+
